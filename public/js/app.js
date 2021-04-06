8e531b6a0179258b166550b519f68798d36d22cd
Nils Edström <nils.edstrom@learnet.se>
2021-04-06 12:02:55 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Clientside java script is loaded')
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = searchElement.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = ''
+                messageTwo.textContent = data.error
+            } else {
+                messageTwo.textContent = ''
+                messageOne.textContent = data. location + '. ' + data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=6cbdedbea793a109eab33323f25cdf8e&query=' +  latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error){
+            callback('Unable to connect to location service!', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees')
+        }
+    })
+}
+
+module.exports = forecast
